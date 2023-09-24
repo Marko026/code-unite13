eade7fc1e9bb08b35524594a34deebd38761bbec
Marko <markec026@yahoo.com>
2023-09-24 14:45:28 +0200

    add auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AskQUestion = () => {
+	return <div>AskQuestion</div>;
+};
+
+export default AskQUestion;
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import 'tailwindcss/tailwind.css';
-export default function Home() {
-	return (
-		<div>
-			<h1 className='text-red-500'>Next.js 13 we are coming</h1>
-		</div>
-	);
-}
