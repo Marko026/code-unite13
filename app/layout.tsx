9f1db25d07596a35f6df7781165f26377e3fb917
Marko <markec026@yahoo.com>
2023-09-25 13:05:14 +0200

    implement themeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import type { Metadata } from 'next';
-import './globals.css';
+import { ThemeProvider } from '@/context/ThemeProvider';
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -20,23 +21,25 @@ export const metadata: Metadata = {
 	title: 'DevFlow',
 	description: 'a community-driven platform for asking and answering questions about programming get help,share kwnolage and colloborate with developers from around  the world ',
 	icons: {
-		icon: '/favicon.ico',
+		icon: '/assets/images/site-logo.svg',
 	},
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<ClerkProvider
-			appearance={{
-				elements: {
-					formButtonPrimary: 'primary-gradient',
-					footerActionLink: 'primary-text-gradient hover-text-primary-500',
-				},
-			}}
-		>
-			<html lang='en'>
-				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
-			</html>
-		</ClerkProvider>
+		<html lang='en'>
+			<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+				<ClerkProvider
+					appearance={{
+						elements: {
+							formButtonPrimary: 'primary-gradient',
+							footerActionLink: 'primary-text-gradient hover-text-primary-500',
+						},
+					}}
+				>
+					<ThemeProvider>{children}</ThemeProvider>
+				</ClerkProvider>
+			</body>
+		</html>
 	);
 }
