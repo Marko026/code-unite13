5ae14e143dd41b0b4c998548d946d256dbed0710
Marko <markec026@yahoo.com>
2023-10-04 19:01:28 +0200

    add public rouths to authMiddelware

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,12 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const AskQUestion = async () => {
-  // const { userId } = auth();
-  const userId = "user2";
+  const { userId } = auth();
+  // const userId = "user2";
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
