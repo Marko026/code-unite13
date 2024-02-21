1f5847620ea1da84e1e7404ddf54dd545e74bacd
MarkoTodorovic <markec026@yahoo.com>
2024-02-21 18:33:08 +0100

    Update API endpoint and request body in chatgpt route

--- app/api/chatgpt/route.tsx
+++ app/api/chatgpt/route.tsx
@@ -3,23 +3,26 @@ import { NextResponse } from "next/server";
 export const POST = async (request: Request) => {
   const { question } = await request.json();
   try {
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+    const response = await fetch(
+      "https://code-unite13-gml3zz1gs-marko026.vercel.app//api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
 
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: `Tell me ${question}` },
+          ],
+        }),
       },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [
-          { role: "system", content: "You are a helpful assistant." },
-          { role: "user", content: `Tell me ${question}` },
-        ],
-      }),
-    });
+    );
     const responseData = await response.json();
 
     const reply = responseData.choices[0].message.content;
