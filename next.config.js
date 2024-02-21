8378a15ada0a5fcd771e0dc2b71e949836b8cb84
MarkoTodorovic <markec026@yahoo.com>
2024-02-21 18:44:19 +0100

    Update API endpoint and add CORS headers

--- next.config.js
+++ next.config.js
@@ -1,22 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- experimental: {
-  serverActions: true,
-  mdxRs: true,
-  serverComponentsExternalPackages: ["mongoose"]
- },
- images: {
-  remotePatterns: [
-   {
-    protocol: "https",
-    hostname: "*",
-   },
-   {
-    protocol: "http",
-    hostname: "*",
-   },
-  ]
- }
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*"
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,POST,PUT,DELETE"
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type"
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                    },
+                ]
+            },
+        ]
+    },
+    experimental: {
+        serverActions: true,
+        mdxRs: true,
+        serverComponentsExternalPackages: ["mongoose"]
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "*",
+            },
+            {
+                protocol: "http",
+                hostname: "*",
+            },
+        ]
+    }
 }
 
 module.exports = nextConfig
