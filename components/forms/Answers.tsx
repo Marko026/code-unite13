edf378bcb9fc327284ebf41b7f52fc12ebe3899e
MarkoTodorovic <markec026@yahoo.com>
2024-02-21 20:19:06 +0100

    Remove "mode: "no-cors" from API call

--- components/forms/Answers.tsx
+++ components/forms/Answers.tsx
@@ -68,7 +68,6 @@ const Answers = ({ question, questionId, authorId }: Props) => {
         `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
         {
           method: "POST",
-          mode: "no-cors",
           body: JSON.stringify({ question }),
         },
       );
