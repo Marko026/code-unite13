383bc7fd1cd81e87809efcb32fe73faf1492bce7
MarkoTodorovic <markec026@yahoo.com>
2024-04-13 14:24:05 +0200

    Update metadata in layout.tsx and page.tsx files

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,36 +35,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <title>DevOverFlow</title>
-      <meta
-        name="description"
-        content="A community-driven platform for asking and answering questions about programming get help,share knowledge and colloborate with developers from around  the world"
-      />
-
-      <meta property="og:url" content="https://code-unite13.vercel.app/" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="DevOverFlow" />
-      <meta
-        property="og:description"
-        content="A community-driven platform for asking and answering questions about programming get help,share knowledge and colloborate with developers from around  the world"
-      />
-      <meta
-        property="og:image"
-        content="https://opengraph.b-cdn.net/production/documents/87af9e85-66b1-44d3-8413-2a107ff2e95f.png?token=Aa5VEkb297BQ91kjV08USY2eiiSWBOfAQD2AtpYrtLg&height=776&width=1200&expires=33249010308"
-      />
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="code-unite13.vercel.app" />
-      <meta property="twitter:url" content="https://code-unite13.vercel.app/" />
-      <meta name="twitter:title" content="DevOverFlow" />
-      <meta
-        name="twitter:description"
-        content="A community-driven platform for asking and answering questions about programming get help,share knowledge and colloborate with developers from around  the world"
-      />
-      <meta
-        name="twitter:image"
-        content="https://opengraph.b-cdn.net/production/documents/87af9e85-66b1-44d3-8413-2a107ff2e95f.png?token=Aa5VEkb297BQ91kjV08USY2eiiSWBOfAQD2AtpYrtLg&height=776&width=1200&expires=33249010308"
-      ></meta>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
         <ClerkProvider
